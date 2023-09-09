bcc10866c83e4bd5f1882278f67c2155178e3786
FunixBoss <89055077+FunixBoss@users.noreply.github.com>
2023-09-10 00:04:23 +0700

    update admin pj base url api

--- admin/src/app/@core/services/base-url.service.ts
+++ admin/src/app/@core/services/base-url.service.ts
@@ -4,7 +4,9 @@ import { Injectable } from "@angular/core";
     providedIn: 'root'
 })
 export class BaseURLService {
-    private _baseURL: string = "http://localhost:9090/api/admin"
+    // private _baseURL: string = "http://localhost:9090/api/admin"
+    private _baseURL: string = "https://crowded-writer-production.up.railway.app/api/admin"
+
     get baseURL(): string{
         return this._baseURL ;
     }
