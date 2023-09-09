952172b212c507ceb7f0814d395d1eb211403114
FunixBoss <89055077+FunixBoss@users.noreply.github.com>
2023-09-10 00:14:14 +0700

    update admin access image directory

--- admin/src/app/@core/services/base-url.service.ts
+++ admin/src/app/@core/services/base-url.service.ts
@@ -1,3 +1,4 @@
+import { ACCOUNT_IMAGE_DIRECTORY } from './../utils/image-storing-directory';
 import { Injectable } from "@angular/core";
 
 @Injectable({
@@ -7,6 +8,7 @@ export class BaseURLService {
     // private _baseURL: string = "http://localhost:9090/api/admin"
     private _baseURL: string = "https://crowded-writer-production.up.railway.app/api/admin"
 
+    ACCOUNT_IMAGE_DIRECTORY
     get baseURL(): string{
         return this._baseURL ;
     }
--- admin/src/app/@core/utils/image-storing-directory.ts
+++ admin/src/app/@core/utils/image-storing-directory.ts
@@ -1,5 +1,7 @@
-export const CATALOG_IMAGE_DIRECTORY: string = 'http://localhost:9090/assets/upload/catalog/' 
-export const PRODUCT_IMAGE_DIRECTORY: string = 'http://localhost:9090/assets/upload/product/' 
-export const VARIANT_IMAGE_DIRECTORY: string = 'http://localhost:9090/assets/upload/variant/' 
-export const ACCOUNT_IMAGE_DIRECTORY: string = 'http://localhost:9090/assets/upload/account/' 
-export const SIDE_GUIDE_IMAGE_DIRECTORY: string = 'http://localhost:9090/assets/upload/size_guide/'
+const baseUrl: string = "https://crowded-writer-production.up.railway.app"
+
+export const CATALOG_IMAGE_DIRECTORY: string =  baseUrl + '/assets/upload/catalog/' 
+export const PRODUCT_IMAGE_DIRECTORY: string =  baseUrl + '/assets/upload/product/' 
+export const VARIANT_IMAGE_DIRECTORY: string =  baseUrl + '/assets/upload/variant/' 
+export const ACCOUNT_IMAGE_DIRECTORY: string =  baseUrl + '/assets/upload/account/' 
+export const SIDE_GUIDE_IMAGE_DIRECTORY: string =  baseUrl + '/assets/upload/size_guide/'
